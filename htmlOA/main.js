89ffda0093e0ae53cb70b195923d4eec3fdae036
Merlinfi21 <88375527+Merlinfi21@users.noreply.github.com>
2021-11-16 10:00:19 +0100

    ss

--- htmlOA/main.js
+++ htmlOA/main.js
@@ -5,3 +5,26 @@ u.addEventListener("click", myFunction)
 function myFunction() {
     console.log()
 }
+
+
+let fun = null;
+
+document.getElementById("framebox").addEventListener("click", function(event) {
+
+
+    if(event.target.tagName !== "INPUT") {
+        return;
+    }
+
+    if(fun != null)
+    fun.classList.remove('pictureToggle');
+
+    if(fun == event.target){
+        fun = null;
+        return;}
+
+    fun = event.target
+
+    event.target.classList.add ("pictureToggle");
+
+});
